3c166421d436eb82d28fdd089f0e111224bb5e31
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-09-01 20:28:50 -0300

    mudança no nome do pacote

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:teste_trabalho/app_widget.dart';
+import 'package:app_nas_gracas_do_imperador/app_widget.dart';
 
 void main() {
   runApp(AppWidget());
