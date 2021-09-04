6b39e80d72140169434efdc6e147a5ca54dbeb20
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-09-03 23:33:13 -0300

    add fundo borrado

--- lib/tutorial.dart
+++ lib/tutorial.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Tutorial extends StatefulWidget {
@@ -11,49 +13,67 @@ class _TutorialState extends State<Tutorial> {
   String text =
       'Este é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial.\nEste é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial.\nEste é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial.\nEste é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial. \nEste é o texto do tutorial.';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Row(
-          children: [
-            Column(
+  Widget _body() {
+    return Container(
+      child: Row(
+        children: [
+          Column(
+            children: [
+              SizedBox(height: 50),
+              Container(
+                alignment: Alignment.bottomCenter,
+                width: 75,
+                child: BackButton(
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/menu');
+                  },
+                ),
+              )
+            ],
+          ),
+          SingleChildScrollView(
+            child: Column(
               children: [
-                SizedBox(height: 50),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  width: 75,
-                  child: BackButton(
-                    color: Colors.black,
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/menu');
-                    },
-                  ),
+                SizedBox(
+                  height: 60,
+                  width: 500,
+                ),
+                Text(
+                  'Tutorial',
+                  style: TextStyle(fontSize: 50),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  text,
+                  style: TextStyle(fontSize: 30),
                 )
               ],
             ),
-            SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 60,
-                    width: 500,
-                  ),
-                  Text(
-                    'Tutorial',
-                    style: TextStyle(fontSize: 50),
-                  ),
-                  SizedBox(height: 30),
-                  Text(
-                    text,
-                    style: TextStyle(fontSize: 30),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg-menu.png"),
+                fit: BoxFit.cover),
+          ),
+        ),
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(color: Colors.grey.shade400.withOpacity(0.2)),
+        ),
+        _body()
+      ],
+    ));
   }
 }
