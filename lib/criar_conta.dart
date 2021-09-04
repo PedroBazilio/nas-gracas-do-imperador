b87075d06627b3824ce4aa86a1627cd85daafec4
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-09-03 23:28:02 -0300

    add fundo borrado

--- lib/criar_conta.dart
+++ lib/criar_conta.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,129 +14,147 @@ class _CriarContaState extends State<CriarConta> {
   String usuario = '';
   String senha = '';
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Crie sua conta',
+            style: TextStyle(fontSize: 30),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 130,
+              ),
+              Text(
+                'Usuário:  ',
+                style: TextStyle(fontSize: 20),
+              ),
+              Flexible(
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: 'Insira seu nome',
+                      border: OutlineInputBorder()),
+                ),
+              ),
+              SizedBox(
+                width: 120,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 130,
+              ),
+              Text(
+                'Senha:    ',
+                style: TextStyle(fontSize: 20),
+              ),
+              Flexible(
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Insira a senha',
+                      border: OutlineInputBorder()),
+                ),
+              ),
+              SizedBox(
+                width: 120,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 450,
+              ),
+              ButtonTheme(
+                  child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/menu');
+                },
+                child: Text(
+                  'Criar conta',
+                  style: TextStyle(fontSize: 15),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green, fixedSize: Size(110, 40)),
+              )),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 70,
+              ),
+              Text(
+                'Já possui uma conta?',
+                style: TextStyle(fontSize: 17),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 100,
+              ),
+              ButtonTheme(
+                  child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/login');
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green, fixedSize: Size(110, 40)),
+              )),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            Text(
-              'Crie sua conta',
-              style: TextStyle(fontSize: 30),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 130,
-                ),
-                Text(
-                  'Usuário:  ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Flexible(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        labelText: 'Insira seu nome',
-                        border: OutlineInputBorder()),
-                  ),
-                ),
-                SizedBox(
-                  width: 120,
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 130,
-                ),
-                Text(
-                  'Senha:    ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Flexible(
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        labelText: 'Insira a senha',
-                        border: OutlineInputBorder()),
-                  ),
-                ),
-                SizedBox(
-                  width: 120,
-                )
-              ],
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 450,
-                ),
-                ButtonTheme(
-                    child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/menu');
-                  },
-                  child: Text(
-                    'Criar conta',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, fixedSize: Size(110, 40)),
-                )),
-              ],
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 70,
-                ),
-                Text(
-                  'Já possui uma conta?',
-                  style: TextStyle(fontSize: 17),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 100,
-                ),
-                ButtonTheme(
-                    child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed('/login');
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.green, fixedSize: Size(110, 40)),
-                )),
-              ],
-            )
-          ],
+        body: Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/bg-menu.png"),
+                fit: BoxFit.cover),
+          ),
         ),
-      ),
-    );
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(color: Colors.grey.shade200.withOpacity(0.2)),
+        ),
+        _body()
+      ],
+    ));
   }
 }
