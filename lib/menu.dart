ee94e4668c501d9466ebd11b8971cbe497f2dad9
gustavocmiranda <56138255+gustavocmiranda@users.noreply.github.com>
2021-09-05 18:45:55 -0300

    add popup para usuario entrar em uma sala

--- lib/menu.dart
+++ lib/menu.dart
@@ -11,105 +11,109 @@ class _MenuState extends State<Menu> {
   String nome = 'Nome do Usuário';
 
   Widget _body() {
-    return Column(children: [
-      SizedBox(height: 30),
-      Row(
-        children: [
-          SizedBox(width: 20),
-          FloatingActionButton(
-            child: Icon(Icons.settings),
-            onPressed: () {
-              Navigator.of(context).pushNamed('/configs');
-            },
-          ),
-          SizedBox(width: 140),
-          Text('Olá, $nome', style: TextStyle(fontSize: 25)),
-          SizedBox(width: 50),
-        ],
-      ),
-      SizedBox(
-        height: 50,
-      ),
-      Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
+    return SingleChildScrollView(
+      child: Column(children: [
+        SizedBox(height: 30),
+        Row(
           children: [
-            SizedBox(
-              width: 30,
+            SizedBox(width: 20),
+            FloatingActionButton(
+              child: Icon(Icons.settings),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/configs');
+              },
             ),
-            ButtonTheme(
-                child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/criarsala');
-              },
-              child: Text(
-                'Criar Sala',
-                style: TextStyle(fontSize: 20),
-              ),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green, fixedSize: Size(180, 70)),
-            )),
-            SizedBox(width: 20),
-            ButtonTheme(
-                child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                'Entrar em Sala',
-                style: TextStyle(fontSize: 20),
-              ),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green, fixedSize: Size(180, 70)),
-            )),
-            SizedBox(width: 20),
-            ButtonTheme(
-                child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/partidarapida');
-              },
-              child: Text(
-                'Partida Rápida',
-                style: TextStyle(fontSize: 20),
-              ),
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.green, fixedSize: Size(180, 70)),
-            ))
+            SizedBox(width: 140),
+            Text('Olá, $nome', style: TextStyle(fontSize: 25)),
+            SizedBox(width: 50),
           ],
         ),
-      ),
-      SizedBox(
-        height: 75,
-      ),
-      Row(
-        children: [
-          SizedBox(width: 40),
-          ButtonTheme(
-              child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/tutorial');
-            },
-            child: Text(
-              'Tutorial',
-              style: TextStyle(fontSize: 15),
-            ),
-            style: ElevatedButton.styleFrom(
-                primary: Colors.green, fixedSize: Size(100, 40)),
-          )),
-          SizedBox(width: 400),
-          ButtonTheme(
-              child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/creditos');
-            },
-            child: Text(
-              'Créditos',
-              style: TextStyle(fontSize: 15),
-            ),
-            style: ElevatedButton.styleFrom(
-                primary: Colors.green, fixedSize: Size(100, 40)),
-          ))
-        ],
-      )
-    ]);
+        SizedBox(
+          height: 50,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 30,
+              ),
+              ButtonTheme(
+                  child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/criarsala');
+                },
+                child: Text(
+                  'Criar Sala',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green, fixedSize: Size(180, 70)),
+              )),
+              SizedBox(width: 20),
+              ButtonTheme(
+                  child: ElevatedButton(
+                onPressed: () {
+                  showAlertDialog(context);
+                },
+                child: Text(
+                  'Entrar em Sala',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green, fixedSize: Size(180, 70)),
+              )),
+              SizedBox(width: 20),
+              ButtonTheme(
+                  child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/partidarapida');
+                },
+                child: Text(
+                  'Partida Rápida',
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.green, fixedSize: Size(180, 70)),
+              ))
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 75,
+        ),
+        Row(
+          children: [
+            SizedBox(width: 40),
+            ButtonTheme(
+                child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/tutorial');
+              },
+              child: Text(
+                'Tutorial',
+                style: TextStyle(fontSize: 15),
+              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.green, fixedSize: Size(100, 40)),
+            )),
+            SizedBox(width: 400),
+            ButtonTheme(
+                child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/creditos');
+              },
+              child: Text(
+                'Créditos',
+                style: TextStyle(fontSize: 15),
+              ),
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.green, fixedSize: Size(100, 40)),
+            ))
+          ],
+        )
+      ]),
+    );
   }
 
   @override
@@ -127,5 +131,33 @@ class _MenuState extends State<Menu> {
         _body()
       ],
     ));
+  }
+
+  showAlertDialog(BuildContext context) {
+    // configura os botões
+    Widget lembrarButton = TextButton(
+      child: Text("Entrar"),
+      onPressed: () {
+        Navigator.of(context).pushNamed('/pregamelobby');
+      },
+    );
+    // configura o  AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Informe o número da sala:"),
+      content: TextField(
+        decoration: InputDecoration(
+            labelText: 'número da sala', border: OutlineInputBorder()),
+      ),
+      actions: [
+        lembrarButton,
+      ],
+    );
+    // exibe o dialogo
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
   }
 }
