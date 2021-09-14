8658b6646864994b226c788e9005d76015e29c6c
Tamires da Hora <thora@id.uff.br>
2021-09-13 22:39:38 -0300

    Base para a tela de partida

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:app_nas_gracas_do_imperador/personagens.dart';
 //import 'package:imagebutton/imagebutton.dart';
 
 String titulo = "Você ganhou!";
@@ -15,7 +16,92 @@ class _PartidaState extends State<Partida> {
   Widget _body() {
     return Scaffold(
       body: Center(
-        child: Column(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 10.0,
+                  ),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 10.0,
+                  ),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 200,
+                    height: 250,
+                    padding: null,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/mapaBrasil.png"))),
+                  ),
+                  Container(
+                    width: 414,
+                    height: 250,
+                    padding: null,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/carta1.png"))),
+                  ),
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 10.0,
+                  ),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              )
+            ],
+          ),
+        ],
+      )
+          /*Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget> [
             TextButton(
@@ -28,35 +114,59 @@ class _PartidaState extends State<Partida> {
                   fontSize: 14,
                   color: Colors.white
                 ),
-              ),
               style: ElevatedButton.styleFrom(
                 primary: Colors.green
               ),
             ),
           ],
-        ),
+        ),*/
+          ),
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: FloatingActionButton(
+              child: Icon(Icons.settings),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/configs');
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              child: Icon(Icons.emoji_events),
+              onPressed: null,
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: FloatingActionButton(
+              child: Icon(Icons.stacked_line_chart_rounded),
+              onPressed: null,
+            ),
+          )
+        ],
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Stack(
-      children: [
-        _body()
-      ],
-    ));
+    return _body();
   }
 
-  Widget _mostraDialogo(BuildContext context){
+  Widget _mostraDialogo(BuildContext context) {
     showDialog(
-      context: context,
-      builder: (BuildContext context){
-        return AlertDialog(
-
-            title: const Text("Você ganhou!", textAlign: TextAlign.center,),
-            content: const Text("XXX é o novo presidente do conselho de ministros"),
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text(
+              "Você ganhou!",
+              textAlign: TextAlign.center,
+            ),
+            content:
+                const Text("XXX é o novo presidente do conselho de ministros"),
             actions: [
               //Menu Principal
               Container(
@@ -64,10 +174,8 @@ class _PartidaState extends State<Partida> {
                 height: 50,
                 padding: null,
                 decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/menuNotPressed.png")
-                  )
-                ),
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/menuNotPressed.png"))),
                 child: TextButton(
                   onPressed: () {
                     Navigator.of(context).pushNamed('/menu');
@@ -75,17 +183,15 @@ class _PartidaState extends State<Partida> {
                   child: const Text(""),
                 ),
               ),
-              
+
               //Jogar Novamente
               Container(
                 width: 200,
                 height: 50,
                 padding: null,
                 decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/menuNotPressed.png")
-                  )
-                ),
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/menuNotPressed.png"))),
                 child: TextButton(
                   onPressed: () {
                     Navigator.of(context).pushNamed('/partida');
@@ -94,9 +200,8 @@ class _PartidaState extends State<Partida> {
                 ),
               ),
             ],
-        );
-      }
-    );
-    throw ''; 
+          );
+        });
+    throw '';
   }
 }
